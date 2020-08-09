7270536d33e6677e1dc32d8132d7c4cd1ea667e1
jaenudin0901 <jaenudinj913198@gmail.com>
2020-08-10 01:00:42 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Css card hover</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="circle">
+                <h2>01</h2>
+            </div>
+            <div class="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, debitis.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="circle">
+                <h2>02</h2>
+            </div>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum enim maxime beatae veniam fuga aliquam dolor officiis vitae aut quod.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="circle">
+                <h2>03</h2>
+            </div>
+            <div class="content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, debitis.</p>
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
